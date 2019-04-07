369938ff688bb966833eb471e1538d587697b24c
Jinhan Park <jinhan92park@gmail.com>
2019-04-07 17:18:36 +0900

    change format

--- config.py
+++ config.py
@@ -1,63 +1,66 @@
 mahn = 10000
 
+
 class MemoryConfig:
-  mem_dir = "save/memory/"
-  memory_size = 50 * mahn
+    mem_dir = "save/memory/"
+    memory_size = 50 * mahn
+
 
 class ModelConfig:
-  model_name = "DQN"
-  in_height = 84
-  in_width = in_height
+    model_name = "DQN"
+    in_height = 84
+    in_width = in_height
 
-  batch_size = 32
-  history_length = 4
+    batch_size = 32
+    history_length = 4
 
-  cnn_archi = [(32, 8, 4),
-               (64, 4, 2),
-               (64, 3, 1)]
-  fc_archi = [512]
+    cnn_archi = [(32, 8, 4), (64, 4, 2), (64, 3, 1)]
+    fc_archi = [512]
 
-  log_dir = "save/tensorboard/"
-  ckpt_dir = "save/ckpt/"
+    log_dir = "save/tensorboard/"
+    ckpt_dir = "save/ckpt/"
+
 
 class EnvironmentConfig:
-  # env_name = "BreakoutDeterministic-v4"
-  env_name = "Breakout-v0"
+    # env_name = "BreakoutDeterministic-v4"
+    env_name = "Breakout-v0"
 
-  max_reward = 1.
-  min_reward = -1.
+    max_reward = 1.
+    min_reward = -1.
+
 
 class TrainConfig:
-  max_step = 5000 * mahn
+    max_step = 5000 * mahn
 
-  initial_exploration = 1.
-  final_exploration = 0.1
-  final_exploration_step = 100 * mahn
+    initial_exploration = 1.
+    final_exploration = 0.1
+    final_exploration_step = 100 * mahn
 
-  test_exploration = 0.05
-  test_play_num = 100
-  
-  replay_start_size = 5 * mahn
-  fixed_net_update_frequency = mahn
+    test_exploration = 0.05
+    test_play_num = 100
 
-  replay_frequency = 4
-  action_repeat = 1
+    replay_start_size = 5 * mahn
+    fixed_net_update_frequency = mahn
 
-  no_op_max = 30
-  
-  df = 0.99
+    replay_frequency = 4
+    action_repeat = 1
 
-  lr = 0.00025
-  lr_min = 0.00025
-  lr_decay = 0.96
-  lr_decay_step = 5 * mahn
+    no_op_max = 30
 
-  save_step = 10 * mahn
-  summarize_step = 5 * mahn
-  load_model = True
-  
+    df = 0.99
+
+    lr = 0.00025
+    lr_min = 0.00025
+    lr_decay = 0.96
+    lr_decay_step = 5 * mahn
+
+    save_step = 10 * mahn
+    summarize_step = 5 * mahn
+    load_model = True
+
+
 class Config(MemoryConfig, ModelConfig, EnvironmentConfig, TrainConfig):
-  load_ckpt = 1
-  test = 0
-  train = 1
-  render = 1
+    load_ckpt = 1
+    test = 0
+    train = 1
+    render = 1
--- main.py
+++ main.py
@@ -5,18 +5,19 @@ from config import Config
 from dqn.agent import Agent
 from dqn.environment import Environment
 
+
 def main():
-  sess = tf.Session()
-  config = Config()
-  env = Environment(config)
-  agent = Agent(sess, config, env)
-  if config.test:
-    agent.play(test=True)
-  elif config.train:
-    agent.train()
-  else:
-    agent.play()
+    sess = tf.Session()
+    config = Config()
+    env = Environment(config)
+    agent = Agent(sess, config, env)
+    if config.test:
+        agent.play(test=True)
+    elif config.train:
+        agent.train()
+    else:
+        agent.play()
 
 
-if __name__=="__main__":
-  main()
+if __name__ == "__main__":
+    main()
